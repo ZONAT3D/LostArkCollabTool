2ed0ee5c6ccfc4f66ffd0ef5879a1117b2b1fe1a
Mark Groe <groemark@gmail.com>
2022-06-11 17:35:01 -0500

    updating some small changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { BrowserRouter } from "react-router-dom";
-
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 
 const darkTheme = createTheme({
