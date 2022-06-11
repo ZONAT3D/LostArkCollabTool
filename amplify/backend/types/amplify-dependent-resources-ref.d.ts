f3d571719a24712afdd772cc37a48b48e562799c
Mark Groe <groemark@gmail.com>
2022-06-11 17:28:42 -0500

    adding backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "lostarkcollabtool": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
